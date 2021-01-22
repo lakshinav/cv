1f021d6b3db468ef16540115e85f5b6a0a6cdbc4
Valeriya Lakshina <lakshinav@users.noreply.github.com>
2021-01-22 14:07:09 +0300

    Update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,42 +1,28 @@
 module.exports = {
-  name: 'Valeriya Lakshina',
-  title: 'PhD HSE, Assoc. Prof.',
+  name: 'Valeriya Lakshina, PhD HSE',
+  title: 'Associate Professor',
+  baseUrl: 'https://lakshinav.github.io/cv/',
   facts: {
-    'Residence': `<a href="https://goo.gl/maps/E3t2hfHBFCitGqzw7">
+    Residence: '<a href="https://goo.gl/maps/E3t2hfHBFCitGqzw7">
                 <i class="fa fa-home fact-icon">  
                 Nizhny Novgorod</i>
-                </a>`,
-    'Homepage': `<a href="https://www.hse.ru/en/staff/lakshinav">
+                </a>',
+    Homepage: '<a href="https://www.hse.ru/en/staff/lakshinav">
                   <span class="print">https://www.hse.ru/en/staff/lakshinav</span>
                   <span class="screen">www.hse.ru</span>
-                </a>`,
+                </a>',
+    GitHub: '<a href="https://github.com/lakshinav"><i class="fab fa-github fact-icon"></i>lakshinav</a>',
+    Email: '<a href="mailto:lakshinav@gmail.com">lakshinav@gmail.com</a>'
 //     'LinkedIn': 
 //     'StackOverflow':
 //     'Kaggle':
 //     'Hackerrank':
-    'GitHub': `<a href="https://github.com/lakshinav/">
-                  <span class="print">https://github.com/lakshinav/</span>
-                  <span class="screen">https://github.com/lakshinav/</span>
-                </a>`,
-    'Email': '<a href="mailto:me">lakshinav@gmail.com</a>'
+    
   },
   skills: [
-    ['Econometrics', '++++'],
-    ['R', '++++'],
-    ['Data Viz', '++++'],
-    
-    ['SQL', '+++'],
-    ['Statistics', '+++'],
-    ['Python', '+++'],
-    
-    ['LaTeX', '+++'],
-    ['knitr', '+++'],
-    ['pandas', '+++'],
-    
-    ['sk-learn', '+++'],
-    ['TPOT', '+++'],
-    ['sktime', '+++'],
-    
+    ['Econometrics', '+++++'], ['Statistics', '++++'], ['R', '+++++'], ['LaTeX', '+++++'], ['knitr', '++++'], ['SQL', '+++'],
+    ['Python', '+++'], ['pandas', '+++'], ['sk-learn', '+++'],  ['TPOT', '+++'], ['sktime', '+++'], ['dash/plotly', '+++']
+    ['CI/CD', '++'], ['ML pipeline', '++']
     // tpot + sktime = AutoML
     // web-scraping
     
@@ -50,43 +36,67 @@ module.exports = {
     // testing: hypothesis
     // CI/CD: GH Actions, Docker
   ],
-  plays: [
+  positions: [
     {
       title: 'HSE University',
       position: 'Associate Professor', 
       written: '2020 - pres.',
       badges: ['teaching', 'research'],
-      contents: `Econometrics and research seminars for bachelor and master students.`
+      contents: 'Econometrics course and research seminars for bachelor and master students.'
     },
     {
       title: 'HSE University',
       position: 'Senior Lecturer', 
       written: '2014 - 2020',
       badges: ['teaching', 'research'],
-      contents: `Econometrics, Macroeconomics I, Game Theory and research seminars for bachelor and master students.`
+      contents: 'Econometrics, Macroeconomics I, Game Theory courses and research seminars for bachelor and master students.'
     },
     {
       title: 'Laboratory of Quantitave Methods in Economics',
       position: 'Research Assistant',
       written: '2013-2014',
       badges: ['research'],
-      contents: `Financial volatility modelling`
+      contents: 'Financial volatility modelling.'
     }
   ],
-  experience: [
+  
+    positions: [ // professional training
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      title: '<a href="https://cs.hse.ru/dpo/announcements/414116513.html">ML pipelines in production</a>',
+      position: 'HSE University and Sber', 
+      written: '11.2020-12.2020',
+      badges: ['kedro', 'fastAPI', 'hypothesis', 'CI/CD', 'Docker', '', '', '', ''],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      This online-course is supported by Russian Federal Project "Human Resources for the Digital Economy".
+      It's devoted to digital transformation of Russian economy and the role of CDO's.
+      `
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      title: 'Introduction to data-driven management',
+      position: 'University of National Technology Initiative 2035', 
+      written: '09.2020-10.2020',
+      badges: ['CDO', 'digital transformation'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ]
+      This online-course is supported by Russian Federal Project "Human Resources for the Digital Economy".
+      It's devoted to digital transformation of Russian economy and the role of CDO's.
+      `
+    },
+    {
+      title: 'Summer digital school: Data Science track',
+      position: 'Sberbank Corporate University', 
+      written: '07.2020-08.2020',
+      badges: ['python', 'pandas', 'keras', 'ML', 'sk-learn'],
+      contents: `
+      Data Science track includes one-week crash-course and 7 additional courses.
+      The crash-course is devoted to in-depth training in data analysis, machine learning algorithms and introduction to neural networks, including CV, NLP and recommerder systems.
+      The courses cover the following topics:
+      * Basics of Programming
+      * Basics of Solving Algorithmic Problems
+      * Basics of SQL
+      * Python for Data Analysis
+      * Machine Learning
+      `
+    },
+  ],
+  
 };
